6d345216640c27fb241b85c0bae99208addce8b9
Karan Kumar <karan.8080dev@gmail.com>
2024-01-03 23:58:36 +0530

    Time: 102 ms (20.90%), Space: 68.9 MB (46.42%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size(); // Determine the size of the input vector 'arr'
         vector<int> v(n); // Create a new vector 'v' of the same size as 'arr' to store modified elements
         
-        int t = -1; // Initialize 't' to -1 (used for last element)
+        //int t = -1; // Initialize 't' to -1 (used for last element)
         v[n - 1] = -1; // Set the last element of 'v' to -1 as specified
         
         // Loop through 'arr' from second-to-last element down to the first element
