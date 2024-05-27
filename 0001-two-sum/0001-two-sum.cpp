3dcb47b0c767381c5808d8f8d0caa51b396be126
Karan Kumar <karan.8080dev@gmail.com>
2024-05-27 15:20:30 +0530

    Time: 9 ms (68.52%), Space: 13.8 MB (41.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) 
     {
+        /* TC = O(n2)
         vector<int> ans;
         int n= arr.size();
         for(int i=0; i<n; i++)
@@ -11,11 +12,52 @@ public:
             {
                 if(t == arr[j])
                 {
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    // ans.push_back(i);
+                    // ans.push_back(j);
+                    return {i,j};
                 }    
             }
         }
-        return ans;
+        // return ans;
+        return{};
+
+        */
+
+
+        map<int,int> mpp;
+        int n= arr.size();
+        for(int i=0; i<n; i++)
+        {
+            int a= arr[i];
+            int more = target - a;
+            if(mpp.find(more) != mpp.end())
+            {
+                return {mpp[more],i};
+            }
+            mpp[a]= i;
+
+        }
+        return {};
+
+ 
+
+
+        /*
+
+        for(int i=0; i<n; i++)
+        {
+            
+        }
+
+        int n= arr.size();
+        int left=0, right= n-1;
+        while(left<right)
+        {
+            int sum = arr[left] + arr[right];
+            if(sum < target)
+                left++;
+            else right --;
+        }
+        */
     }
 };
