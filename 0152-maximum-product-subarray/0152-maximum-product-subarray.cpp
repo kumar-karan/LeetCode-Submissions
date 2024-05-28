3e3a3a40544425ff36a229c449847ab1a88d8fb7
Karan Kumar <karan.8080dev@gmail.com>
2024-05-28 10:04:56 +0530

    Time: 3 ms (85.90%), Space: 16.2 MB (46.07%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,15 +4,42 @@ public:
     {
         int n= nums.size();
         int maxi = INT_MIN;
-        for(int i=0; i<n; i++)
-        { int prod = 1;
-            for(int j=i; j<n; j++)
-            {
+        // for(int i=0; i<n; i++)
+        // { int prod = 1;
+        //     for(int j=i; j<n; j++)
+        //     {
                 
-                prod*= nums[j]; // yha mistake hui thi
-                maxi= max(maxi, prod);
-            }
+        //         prod*= nums[j]; // yha mistake hui thi
+        //         maxi= max(maxi, prod);
+        //     }
+        // }
+        // return maxi;
+
+/*
+        int prod=1;
+        for(int i=0; i<n; i++)
+        {
+            prod *= nums[i];
+            maxi= max(prod,maxi);
+            if(prod <= 0)
+                prod = 1;
         }
+        return maxi;
+        
+*/
+
+        int pre =1, suff=1;
+        for(int i=0; i<n;i++)
+        {
+            if(pre == 0) pre =1;
+            if(suff == 0) suff =1;
+
+            pre*=nums[i];
+            suff*=nums[n-i-1];
+
+            maxi = max(maxi, max(pre,suff));
+        }
+
         return maxi;
     }
 };
