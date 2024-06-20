78af934f44c5a4ea50eb47382eb4a7b0298aa6c2
Karan Kumar <karan.8080dev@gmail.com>
2024-06-20 16:31:35 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (73.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    double myPow(double x, int n) 
+    // double my1Pow(double x, int n) 
+    // {
+    //     if(n == 0)
+    //         return 1;
+    //     if(n<0)
+    //     {
+    //         n = abs(n);
+    //         x = 1/x;
+    //     }
+    //     if(n%2 == 0)
+    //         // return myPow(x*x, n/2);
+    //     // else return(x*myPow(x,n-1));
+    // }
+
+    double myPow(double x, int n)
     {
-        if(n == 0)
-            return 1;
+        if(n==0)    return 1;
+
         if(n<0)
         {
             n = abs(n);
             x = 1/x;
         }
-        if(n%2 == 0)
+        if(n%2==0)
             return myPow(x*x, n/2);
-        else return(x*myPow(x,n-1));
+        else return(x * myPow(x,n-1));
     }
 };
