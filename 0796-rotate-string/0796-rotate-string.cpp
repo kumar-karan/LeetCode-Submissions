8d65df1ac89ac13a4aee978663c5eceb6b646a66
Karan Kumar <karan.8080dev@gmail.com>
2024-06-24 23:45:26 +0530

    Time: 0 ms (100%), Space: 7.3 MB (62.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -4,13 +4,14 @@ public:
     {
         int n =s.size(); int m = goal.size();
         if(n != m)  return false;
+        if(s == goal) return true;
 
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n-1; i++)
         {
             char ch = s[0];
             for(int j=0; j<n-1; j++)
                 s[j] = s[j+1];
-                
+
             s[n-1] = ch;
             if(s == goal) return true;
         }
