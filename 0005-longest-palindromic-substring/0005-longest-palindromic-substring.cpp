6f9aef9533316ee1385a560c48b876bb8bc7b9b6
Karan Kumar <karan.8080dev@gmail.com>
2024-06-25 23:21:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& dp, string &s) {
-        if (i==j || abs(i-j) == 1) {
-            if (s[i] == s[j]) {
-                return abs(i-j) + 1;
-            }
-            return 0;
-        }
-        if (dp[i][j]!=-1) {
-            return dp[i][j];
-        }
-        int p = solve(i+1, j-1, dp, s);
-        if (p) {
-            if (s[i] == s[j]) {
-                p = p + 2;
-            } else {
-                p = 0;
-            }
-        }
-        
-        dp[i][j] = p;
-        return p;
+    bool solve(string s, int i, int j)
+    {
+        if(i>=j)    return true;
+
+        if(s[i] == s[j]) return solve(s,i+1,j-1);
+
+        return false;
     }
-    string longestPalindrome(string s) {
-        vector<vector<int>> v(s.size(), vector<int> (s.size(), -1));
-        int largest = 0;
-        int a = 0, b = 0;
-        for(int i=0;i<s.size();i++) {
-            for(int j=i;j<s.size();j++) {
-                int t = solve(i, j, v, s);
-                if (t>largest) {
-                    largest = t;
-                    a = i;
-                    b = j;
+    string longestPalindrome(string s) 
+    {
+        int maxi = 0;
+        int n = s.size();
+        int sp=0;
+
+        for(int i=0; i< n; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                if(solve(s,i,j) == true)
+                {
+                    if(j-i+1 > maxi)
+                    {
+                        maxi = j-i+1;
+                        sp=i;
+                    }
                 }
             }
         }
-        
-        return s.substr(a, b-a+1);
+
+        return s.substr(sp,maxi);
     }
 };
-
-
-        
-
