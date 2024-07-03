a1828b45e37c3f2549bd590ec74eacec66c8a6aa
Karan Kumar <karan.8080dev@gmail.com>
2024-07-03 22:08:25 +0530

    Time: 75 ms (24.45%), Space: 38.9 MB (15.52%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -7,6 +7,6 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        return nums[n-3]-nums[0];
+        return min({nums[n-4]-nums[0], nums[n-1]-nums[3], nums[n-2]-nums[2], nums[n-3]-nums[1]});
     }
 };
